16f6e21c109bda3019d2cb7376cb2c03c88973cf
Kenny Pflug <kenny.pflug@live.de>
2017-03-26 09:17:19 +0200

    added example for modularity in JavaScript

---
+++ Code/JavaScriptModules/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace JavaScriptModules
+{
+    public sealed class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+               .UseStaticFiles();
+        }
+    }
+}
